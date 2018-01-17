ca99d92fba0723bc5b6c6e8c42145e6f38ab4882
lemeryfertitta <lemeryfertitta@gmail.com>
2018-01-17 10:08:27 -0800

    Increment version in setup.py to 0.2.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='sqlalchemy-vertica-python',
-    version='0.1.3',
+    version='0.2.0',
     description='Vertica dialect for sqlalchemy using vertica_python',
     long_description=open("README.rst").read(),
     license="MIT",
     url='https://github.com/LocusEnergy/sqlalchemy-vertica-python',
-    download_url = 'https://github.com/LocusEnergy/sqlalchemy-vertica-python/tarball/0.1.3',
+    download_url = 'https://github.com/LocusEnergy/sqlalchemy-vertica-python/tarball/0.2.0',
     author='Locus Energy',
     author_email='dbio@locusenergy.com',
     packages=[

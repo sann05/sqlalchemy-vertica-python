9fd50a38c84bb638dc1de0b65cb291a2f400e03b
vinceatbluelabs <vince.broz@bluelabs.com>
2020-02-20 08:48:54 -0500

    Fix CircleCI context for deploy (#5)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuputils import TestCoverageRatchetCommand, VerifyVersionCommand
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 setup(
     name='sqlalchemy-vertica-python',
     version=__version__,

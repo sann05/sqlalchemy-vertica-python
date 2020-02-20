5d367042a27e4758965405d5dba943630abbb4fb
vinceatbluelabs <vince.broz@bluelabs.com>
2020-02-20 08:37:40 -0500

    Add twine requirement, needed for deploy job in CircleCI (#4)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuputils import TestCoverageRatchetCommand, VerifyVersionCommand
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 setup(
     name='sqlalchemy-vertica-python',
     version=__version__,

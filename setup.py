014cb1dcab8bd9d117773fd2511f94a14611bc19
Vince Broz <vince.broz@bluelabs.com>
2020-02-20 11:42:14 -0500

    Move BlueLabs info to maintainer fields

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     url='https://github.com/bluelabsio/sqlalchemy-vertica-python',
     download_url = 'https://github.com/bluelabsio/sqlalchemy-vertica-python/tarball/{}'.format(__version__),
     author='Luke Emery-Fertitta',
-    author_email='opensource@bluelabs.com',
+    maintainer='Vince Broz',
+    maintainer_email='opensource@bluelabs.com',
     packages=[
         'sqla_vertica_python',
     ],
